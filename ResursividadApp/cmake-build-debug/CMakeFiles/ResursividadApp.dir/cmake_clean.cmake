4651003c06fbf680debe3153802a5880bc9440e5
Sebastian Alvarez <>
2019-10-15 08:01:54 -0500

    Trabajo en clase

---
+++ ResursividadApp/cmake-build-debug/CMakeFiles/ResursividadApp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ResursividadApp.dir/main.cpp.obj"
+  "ResursividadApp.pdb"
+  "ResursividadApp.exe"
+  "ResursividadApp.exe.manifest"
+  "ResursividadApp.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ResursividadApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
