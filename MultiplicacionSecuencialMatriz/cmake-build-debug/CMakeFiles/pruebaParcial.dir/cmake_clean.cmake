661c3f503b8bd4ab499d477ca5d26d5ebb8c049f
Sebastian Alvarez <>
2019-10-20 22:10:14 -0500

    Prueba Primer Parcial (Corregida)

---
+++ MultiplicacionSecuencialMatriz/cmake-build-debug/CMakeFiles/pruebaParcial.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pruebaParcial.dir/main.cpp.obj"
+  "pruebaParcial.pdb"
+  "pruebaParcial.exe"
+  "pruebaParcial.exe.manifest"
+  "pruebaParcial.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pruebaParcial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
