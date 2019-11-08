ce738691272f2c0d64b78636ad74684bff17ff45
Sebastian Alvarez <44406603+jsalvarez44@users.noreply.github.com>
2019-11-07 21:05:54 -0500

    Mejora al proyecto FactorialApp
    -cambio de IDE y compilador
    -aplicacion de la funcion recursiva

---
+++ ResursividadApp/FactorialRecursivo/Main.cpp
@@ -0,0 +1,21 @@
+/**
+ *               UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE
+ *                  CARRERA DE INGENIERIA DE SOFTWARE
+ * NOMBRE:                 SEBASTIAN ALVAREZ
+ * FECHA DE ELABORACION:   30 - SEP - 2019
+ * FECHA DE MODIFICACION:  30 - SEP - 2019
+ */
+#include <iostream>
+#include "Dato.h"
+#include "Ingreso.h"
+
+int main() {
+	Ingreso ingreso;
+	Dato n_dato(ingreso.ingresar("Ingrese un numero entero : "));
+
+	int factorial = n_dato.procesar(n_dato.getValor());
+
+	n_dato.toString();
+	std::cout << "Factorial: " << factorial << std::endl;
+	return 0;
+}
